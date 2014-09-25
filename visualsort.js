e52d2fa92c402d78793eceb7ac6d0a85fe4231fe
Justin <justin.tyler.coon@gmail.com>
2014-09-25 13:15:14 -0400

    finished implementing visual bubble sort

--- visualsort.js
+++ visualsort.js
@@ -2,19 +2,36 @@
 var context = document.getElementById("a").getContext("2d");
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
-setInterval(draw, 25);
+setInterval(draw, 1);
 
 var num = 100;
 var values = [];
 for(var x = 0; x < num; x++)
 	values.push(Math.random());
 
+var place = 0;
+
 function draw()
 {
 	context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	for(var x = 0; x < values.length; x++)
 		context.fillRect((x/num)*window.innerWidth, window.innerHeight, window.innerWidth/num, -values[x]*window.innerHeight);
-	context.fillRect(player.x-5, player.y-5, 10, 10);
+	for(var a = 0; a < 10; a++)
+		step();
+}
+function step()
+{
+	if(values[place] > values[place+1])
+		swap(values, place, place+1);
+	place++;
+	if(place >= num-1)
+		place = 0;
+}
+function swap(vals, p1, p2)
+{
+	var temp = vals[p1];
+	vals[p1] = vals[p2];
+	vals[p2] = temp;
 }
 
 /*document.addEventListener('keyup', function (event) {
